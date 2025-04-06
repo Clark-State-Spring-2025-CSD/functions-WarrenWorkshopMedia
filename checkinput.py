0d6bac3cc7fac76a5af9e0e8a2c4f2ec8037f71e
Alex Warren <WarrenWorkshopMedia@gmail.com>
2025-04-06 12:14:27 -0400

    Completed

--- checkinput.py
+++ checkinput.py
@@ -4,3 +4,19 @@
 #The function will continue to prompt the user to enter an integer until a proper integer is entered.
 #The most direct way of doing this would be using a try block, which has not been covered yet. You will need to research this.
 #Write supporting code to call the function, and then display the number that was entered.
+
+
+#The function to check the user's input is valid or not.
+def CheckUserInput(Prompt, ErrorMessage):
+    #While loop so they keep the program running until they enter a number!
+    while True:
+        UserInput = input(Prompt)
+        #The try-block will return the input except if the user wants to try and type letters...
+        try:
+            return int(UserInput)
+        except ValueError:
+            print(ErrorMessage)
+
+#Display either the number entered or the error message for the inputs.
+Number = CheckUserInput("Enter a number: ", "Invalid input. Please enter a valid integer.")
+print(f"You entered: {Number}")
--- rolldice.py
+++ rolldice.py
@@ -19,3 +19,27 @@
 # How many sides? 20
 # Here are the results: [18, 19, 6, 8, 13, 6, 6, 6, 18, 12, 20, 10, 14, 8, 14, 17, 12, 15, 20, 17]
 
+#Random Number Generator and seed for testing
+import random
+
+random.seed()
+
+ #Check the User input and generate the list of random dice rolls.
+def Roll_Dice(Num_Dice, Num_Sides):
+    #Display error message in case the user wants to be funny attempting to roll 0 dice or a 1 sided coin.
+    if Num_Dice <= 0 or Num_Sides <= 1:
+        raise ValueError("Sides must be greater than 1 and dice count greater than 0.")
+    return [random.randint(1, Num_Sides) for _ in range(Num_Dice)]
+
+#Try block to prompt user for input of the number of dice and sides of the dice.
+try:
+    Num_Dice = int(input("How many dice to roll?: "))
+    Num_Sides = int(input("How many sides?: "))
+#Call the roll the dice function and display the results for the user's input
+    Dice_Results = Roll_Dice(Num_Dice, Num_Sides)
+    print(f"Here are the results: {Dice_Results}")
+#Display the error message if user's input was invalid...
+except ValueError as Error:
+    print(f"Invalid Input: {Error}")
+
+
